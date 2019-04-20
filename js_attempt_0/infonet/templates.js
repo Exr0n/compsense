fefd1aca230e00fd66068cab05590856f29a4448
Exr0n <spotyie@gmail.com>
2019-04-19 22:52:14 -0700

    blog post: "first revalation (maybe?)"; js updates

--- js_attempt_0/infonet/templates.js
+++ js_attempt_0/infonet/templates.js
@@ -2,30 +2,38 @@
 
 exports.Network = class {
   constructor (app, json) {
+    this.app = app; // store reference to the application, if we ever need it
 
-    this.app = app;
+    this.concepts = {}; // start from scratch
+    this.relations = {};
 
-    if (json) {
-      this.concepts = {}
-      for (let key in json.concepts) this.concepts[key] = new exports.Concept( json.concepts[key] );
-    } else {
-      this.concepts = {};
-      this.relations = {};
+    if (json) { // if we are importing a past "save"
+      for (let key in json.concepts) this.concepts[key] = new exports.Concept( this, json.concepts[key] );
+      for (let key in json.relations) this.relations[key] = new exports.Relation( this, json.relations[key] );
     }
   }
 
-  exportJson
+  exportJson () {
+    let end = {
+      concepts: {},
+      relations: {}
+    };
 
-  /*
-  self = {
-    nodes: {}
-  };
-  return self;
-  */
+    for (let key in this.concepts) end.concepts[key] = this.concepts[key].exportJson();
+    for (let key in this.relations) end.relations[key] = this.relations[key].exportJson();
+
+    return JSON.stringify(end, (k,v)=>v, 2); // make the end json string readable to humans
+  }
 }
 
-/*
-exports.Network.prototype = {
+exports.Relatable = class {
+  constructor (network, json) {
+    this.network = network;
+
+    for (let concept of )
+  }
+}
+
+exports.Concept = class {
 
 }
-*/
