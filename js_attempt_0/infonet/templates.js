f1421053f3f615091422fba08acc33d27af5ecf5
Exr0n <spotyie@gmail.com>
2019-04-19 23:23:36 -0700

    typo changes in progress_log; js updates

--- js_attempt_0/infonet/templates.js
+++ js_attempt_0/infonet/templates.js
@@ -1,31 +1,34 @@
 "use strict";
 
 exports.Network = class {
+  /*
+  The network itself
+  Knows about all the nodes, as well as some config type stuff
+  */
   constructor (app, json) {
     this.app = app; // store reference to the application, if we ever need it
 
     this.concepts = {}; // start from scratch
-    this.relations = {};
 
     if (json) { // if we are importing a past "save"
-      for (let key in json.concepts) this.concepts[key] = new exports.Concept( this, json.concepts[key] );
-      for (let key in json.relations) this.relations[key] = new exports.Relation( this, json.relations[key] );
+      for (let key in json.concepts) {
+        if (/* todo: concept already exists */false) {}
+        this.concepts[key] = new exports.Concept( this, json.concepts[key] );
+      }
     }
   }
 
   exportJson () {
     let end = {
-      concepts: {},
-      relations: {}
+      concepts: {}
     };
 
     for (let key in this.concepts) end.concepts[key] = this.concepts[key].exportJson();
-    for (let key in this.relations) end.relations[key] = this.relations[key].exportJson();
 
     return JSON.stringify(end, (k,v)=>v, 2); // make the end json string readable to humans
   }
 }
-
+/* This is a remnant from when concepts and relations were different things.
 exports.Relatable = class {
   constructor (network, json) {
     this.network = network;
@@ -33,7 +36,36 @@ exports.Relatable = class {
     for (let concept of )
   }
 }
-
+*/
 exports.Concept = class {
+  /*
+  Some kind of idea that the network knows about
+  */
+  constructor (network, json) {
+    this.network = network;
 
+    for (let connect of json)
+  }
+}
+
+exports.Relation = class {
+  /*
+  The connection between two concepts
+  */
+  constructor (network, start, end, routes) {
+    this.network = network;
+
+
+  }
+}
+
+exports.Route = class {
+  /*
+  A specific way in which two concepts are connected
+  */
+  constructor (network, start, end, path) {
+    this.network = network;
+    
+
+  }
 }
