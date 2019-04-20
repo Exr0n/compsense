8f7819653677544740aeb402d4f00a3f9d684103
Exr0n <spotyie@gmail.com>
2019-04-19 23:40:57 -0700

    jsupdates

--- js_attempt_0/infonet/templates.js
+++ js_attempt_0/infonet/templates.js
@@ -11,10 +11,8 @@ exports.Network = class {
     this.concepts = {}; // start from scratch
 
     if (json) { // if we are importing a past "save"
-      for (let key in json.concepts) {
-        if (/* todo: concept already exists */false) {}
-        this.concepts[key] = new exports.Concept( this, json.concepts[key] );
-      }
+      for (let key in json.concepts) this.concepts[key] = new exports.Concept( this, name ); // create all the concepts
+      /* todo: import the connections */
     }
   }
 
@@ -25,7 +23,7 @@ exports.Network = class {
 
     for (let key in this.concepts) end.concepts[key] = this.concepts[key].exportJson();
 
-    return JSON.stringify(end, (k,v)=>v, 2); // make the end json string readable to humans
+    return end;
   }
 }
 /* This is a remnant from when concepts and relations were different things.
@@ -41,21 +39,49 @@ exports.Concept = class {
   /*
   Some kind of idea that the network knows about
   */
-  constructor (network, json) {
+  constructor (network, name) {
     this.network = network;
+    this.name = name;
 
-    for (let connect of json)
+    this.relations = [];
+  }
+
+  connect (other) {
+    /* todo Form a connection between this and another concept */
+  }
+
+  exportJson () {
+    let end = {
+      relations: []
+    }
+    for (let rel of this.relations) end.relations.push( rel.exportJson() );
+    return end;
   }
 }
 
 exports.Relation = class {
   /*
-  The connection between two concepts
+  A connection between two concepts
   */
-  constructor (network, start, end, routes) {
+  constructor (network, start, end) {
     this.network = network;
+    this.start = start;
+    this.end = end;
 
+    this.routes = [];
+    this.strength = 0;
+  }
 
+  addRoute (path) {
+    /* todo Create a new route via concepts */
+  }
+
+  exportJson () {
+    let end = {
+      routes: []
+    }
+    for (let route of this.routes) end.push( route.exportJson() );
+    return end;
   }
 }
 
@@ -65,7 +91,13 @@ exports.Route = class {
   */
   constructor (network, start, end, path) {
     this.network = network;
-    
 
+    this.path = path;
+    this.strength = undefined; // todo: what does this even mean, how will it work? via points themselves may be unrelated
+  }
+
+  exportJson () {
+    var end = [];
+    for (let concept of path) end.push( )
   }
 }
